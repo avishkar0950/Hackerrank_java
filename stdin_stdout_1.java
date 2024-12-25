fe86e9e55e6716050cf2c4bdaf5bb04759fd537a
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-12-26 01:12:20 +0530

    Add files via upload

---
+++ stdin_stdout_1.java
@@ -0,0 +1,20 @@
+package practice_program;
+import java.util.*;
+public class stdin_stdout_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+        Integer i= scan.nextInt();
+        Double d = scan.nextDouble();
+        String s = scan.next();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int:"+i);
+
+	}
+
+}                          
